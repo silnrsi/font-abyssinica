837087898291f308d402fbe48df1cdbff177a3b1
Lorna Evans <LornaSIL@users.noreply.github.com>
2024-10-14 08:53:12 -0500

    Update source documentation. [nobuild]

--- documentation/source/pandocfilters/filter-html.lua
+++ documentation/source/pandocfilters/filter-html.lua
@@ -1,9 +1,10 @@
 function Link(el)
-  el.target = string.gsub(el.target, "%.md", ".html")
+  if string.find(el.target, "^https?://") == nil then
+    el.target = string.gsub(el.target, "%.md", ".html")
+  end
   el.target = string.gsub(el.target, "%.rawmd", ".md")
   return el
 end
-
 
 function Image(el)
   el.src = string.gsub(el.src, "../assets", "assets")
